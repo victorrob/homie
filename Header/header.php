96fa8c53164d8b659eb5be764cc1c8d084687cca
henri <victor@gmail.com>
2018-03-23 14:40:43 +0100

    add header.php

---
+++ Header/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8" />
+    <header>
+        <img src="../image/LogoHOMIEweb.png" alt="Logo Homie" />
+        <div id="roue">
+            <img src="../image/parametre.png" alt="Bouton paramètre" id="parametre" />
+            <ul id="menu">
+                <li><a href="#">Paramètres</a></li>
+                <li><a href="#">Nous contacter</a></li>
+                <li><a href="#">Déconnexion</a></li>
+            </ul>
+        </div>
+    </header>
+</html>

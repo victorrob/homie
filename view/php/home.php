b00357713f1b8120bb64862b50af28498bf8275e
victor <victorrrobert@gmail.com>
2018-04-03 12:04:41 +0200

    ajout de la page header et actualisation de la page home

--- view/php/home.php
+++ view/php/home.php
@@ -1,37 +1,18 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="view/css/home.css" />
-		<title>accueil</title>
-	</head>
-	<body>
-		<header>
-			<a href="index.php?p=home" <img src="image/LogoHOMIEweb.png" alt="Logo Homie" />
-		</header>
 		<div id="tete" class="menu_tete">
-				<form class="form" method="post" action="index.php">
+				<form class="form" method="post" action="/index.php">
 					<label for="habitation">Habitation :</label><br />
 					<select name="habitation" id="habitation">
 						<option value="maison_brest">Maison Brest</option>
 						<option value="appartement_paris">Appartement Paris</option>
 					</select>
 				</form>
-			<div id="roue">
-				<img src="image/parametre.png" alt="Bouton paramètre" id="parametre" />
-					<ul id="menu">
-						<li><a href="index.php?p=profile">Paramètres</a></li>
-						<li><a href="index.php?p=contact">Nous contacter</a></li>
-						<li><a href="index.php?p=logOut">Déconnexion</a></li>
-					</ul>
-			</div>
-		</div>
+        </div>
 		<div class="entete">
 			<hr />
 			<h1>Pièces</h1>
 			<hr />
 		</div>
-		<section>
+		<section id="roomSection">
 			<div><p>Cuisine</p></div>
 			<div><p>Chambre 1</p></div>
 			<div><p>Salon</p></div>
@@ -43,13 +24,12 @@
 			<h1>Habitation</h1>
 			<hr />
 		</div>
-		<section>
+		<section id="habitationSection">
 			<div><p>Lumières</p></div>
 			<div><p>Volets</p></div>
 			<div><p>Absent</p></div>
 		</section>
 
-		<script type="text/javascript" src="view/js/home.js"></script>
+		<script type="text/javascript" src="/view/js/header.js"></script>
 
 	</body>
-</html>

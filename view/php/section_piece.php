2a57b091841c7ac457f50460af0c6ca6ab56ed7d
ValentinVr <rouleau.valentin@gmail.com>
2018-04-09 14:51:47 +0200

    passage des sections de flex a grid.

--- view/php/section_piece.php
+++ view/php/section_piece.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Utilisateur
- * Date: 08/04/2018
- * Time: 17:58
- */
+<head>
+    <link rel="stylesheet" href="view/css/section_piece.css" />
+</head>
+
+<html>
+    <section id="piece">
+        <div><p>Cuisine</p></div>
+        <div id="ligne">
+            <div>
+                <p>Lumière :</p>
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                </label>
+            </div>
+            <div><p>Chauffage :</p></div>
+            <div><p>Ventilation :</p></div>
+        </div>
+        <div><p>Volets :</p></div>
+        <div><p>Plage horaire :</p></div>
+        <div><a href="#"><p>Statistiques</p></a></div>
+    </section>
+</html>

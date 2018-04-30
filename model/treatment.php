749725005255839ef6d6ffad64e77bc3a965eb9a
henri <victor@gmail.com>
2018-04-30 14:30:40 +0200

    correction faille XSS treatment.php function signUp

--- model/treatment.php
+++ model/treatment.php
@@ -55,57 +55,57 @@ function getHistoric($roomId, $PDO){
 //add user
 function signUp($PDO){
     if (isset($_POST['name']))
-        $name=$_POST['name'];
+        $name= strip_tags($_POST['name']);
     else
         $name="";
 
     if (isset($_POST['firstName']))
-        $firstName=$_POST['firstName'];
+        $firstName=strip_tags($_POST['firstName']);
     else
         $firstName="";
 
     if (isset($_POST['mail']))
-        $mail=$_POST['mail'];
+        $mail=strip_tags($_POST['mail']);
     else
         $mail="";
 
     if (isset($_POST['phone']))
-        $phone=$_POST['phone'];
+        $phone= strip_tags($_POST['phone']);
     else
         $phone="";
 
     if (isset($_POST['password']))
-        $password=$_POST['password'];
+        $password= strip_tags($_POST['password']);
     else
         $password="";
 
     if (isset($_POST['type']))
-        $type=$_POST['type'];
+        $type=strip_tags($_POST['type']);
     else
         $type="";
 
     if (isset($_POST['birthDate']))
-        $birthDate=$_POST['birthDate'];
+        $birthDate= strip_tags($_POST['birthDate']);
     else
         $birthDate="";
 
     if (isset($_POST['address']))
-        $address=$_POST['address'];
+        $address= strip_tags($_POST['address']);
     else
         $address="";
 
     if (isset($_POST['zipCode']))
-        $zipCode=$_POST['zipCode'];
+        $zipCode= strip_tags($_POST['zipCode']);
     else
         $zipCode="";
 
     if (isset($_POST['city']))
-        $city=$_POST['city'];
+        $city= strip_tags($_POST['city']);
     else
         $city="";
 
     if (isset($_POST['country']))
-        $country=$_POST['country'];
+        $country= strip_tags($_POST['country']);
     else
         $country="";
 

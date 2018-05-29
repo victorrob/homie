b4092c9442583eefc5633659e322ee7a85fa848e
Alessandro <alessandro.rossi@isep.fr>
2018-05-29 11:51:38 +0200

    correction erreur text profile

--- view/php/profile.php
+++ view/php/profile.php
@@ -11,21 +11,21 @@
 					<label for="name">Nom : <?php echo($name); ?></label>
 					<input class="button" type="button" value="Modifier" id="nameButton"/>
 				</div>
-				<input id="nameModif" type="text" placeholder="Nouvau nom" name="name" />
+				<input id="nameModif" type="text" placeholder="Nouveau nom" name="name" />
 			</div>
 			<div id="firstName" class="categoryForm">
 				<div>
 					<label for="firstName">Prénom : <?php echo($firstName); ?></label>
 					<input class="button" type="button" value="Modifier" id="firstNameButton"/>
 				</div>
-				<input id="firstNameModif" type="text" placeholder="Nouvau prénom" name="firstName" />
+				<input id="firstNameModif" type="text" placeholder="Nouveau prénom" name="firstName" />
 			</div>
 			<div id="birth" class="categoryForm">
 				<div>
-					<label for="birth">Date d'aniversaire : <?php echo($birthDate); ?></label>
+					<label for="birth">Date d'anniversaire : <?php echo($birthDate); ?></label>
 					<input class="button" type="button" value="Modifier" id="birthButton"/>
 				</div>
-				<input id="birthModif" type="date" placeholder="Nouvelle date d'anniversair" name="birth" />
+				<input id="birthModif" type="date" placeholder="Nouvelle date d'anniversaire" name="birth" />
 			</div>
 			<div id="email" class="categoryForm">
 				<div>
@@ -46,14 +46,14 @@
 					<label for="phone">Téléphone : <?php echo($phone); ?></label>
 					<input class="button" type="button" value="Modifier" id="phoneButton"/>
 				</div>
-				<input id="phoneModif" type="tel" placeholder="Nouvau téléphone" name="phone" />
+				<input id="phoneModif" type="tel" placeholder="Nouveau téléphone" name="phone" />
 			</div>
 			<div id="password" class="categoryForm">
 				<div>
 					<label for="password">mot de passe : ••••••••••</label>
 					<input class="button" type="button" value="Modifier" id="passwordButton"/>
 				</div>
-				<input id="password1Modif" type="password" placeholder="Nouvau mot de passe" name="password1" />
+				<input id="password1Modif" type="password" placeholder="Nouveau mot de passe" name="password1" />
 				<input id="password2Modif" type="password" placeholder="Confirmer mot de passe" name="password2" />
 			</div>
 			<div id="validate" class="categoryForm">
@@ -61,8 +61,8 @@
 				<input class="button" type="submit" value="Valider" id="validateButton"/> <!-- submit -->
 			</div>
 		</form>
-		<form action="index.php?p=profile">
-			<input class="button" type="submit" value="Anuler" id="validateButton"/> <!-- anuler -->
+		<form method="POST" action="index.php?p=home">
+			<input class="button" type="submit" value="Annuler" id="validateButton"/> <!-- anuler -->
 		</form>
 	</fieldset>
 </body>

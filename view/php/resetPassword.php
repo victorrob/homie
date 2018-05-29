7584553e2e846ef225543112fc268a51ca2b90a0
abdou <abderrahmane.rhmari@gmail.com>
2018-05-29 11:28:33 +0200

    cacher les passwords

--- view/php/resetPassword.php
+++ view/php/resetPassword.php
@@ -11,9 +11,9 @@
    <form method="post" action=<?php echo("index.php?p=ChangePswdOk&d=resetPassword&h=".$h); ?> >
        <div class="erreurPswd"> <?php echo $erreurEgalPswd ?> </div>
         <p><label>Nouveau mot de passe :</label></p>
-        <p><input class="complete largeText" type="text" name="newPassword" required /></p>
+        <p><input class="complete largeText" type="password" name="newPassword" required /></p>
         <p><label>Confirmer le mot de passe :</label></p>
-        <p><input class="complete largeText" type="text" name="newPassword2" required /></p>
+        <p><input class="complete largeText" type="password" name="newPassword2" required /></p>
 
         <p><input type="submit" name="okPassword" value="OK" /></p>
     </form>

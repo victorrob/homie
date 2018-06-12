4247f380be60a00d9985b00fe295e116789c40ae
ValentinVr <rouleau.valentin@gmail.com>
2018-06-12 17:07:42 +0200

    JS + HOME fonctionnel

--- view/js/home.js
+++ view/js/home.js
@@ -4,7 +4,7 @@ $(function () {
             $('.value', this).text('[Problème]');
             $('.value', this).css('color', 'red');
             $('.unité', this).css('visibility', 'hidden');
-            $(this).css('width', '500px');
+            $(this).css('width', '450px');
             $(('.value', this)).mouseover(function () {
                 $('.value', this).text('La liaison entre le capteur et la base de données est impossible');
                 $('.value', this).css('color', 'white');
@@ -17,66 +17,47 @@ $(function () {
             });
         }
     });
-});
-
-function home(id, className) {
-    var form = document.getElementById(id);
-    var forms = document.getElementsByClassName(className);
-    if (form.style.display === 'flex') {
-        form.style.display = 'none';
-    }
-    else {
-        for (var i=0; i<forms.length; i++) {
-            forms[i].style.display = 'none';
+    $('.carre').click(function () {
+        var href = $(this).attr('href');
+        if ($(href).css('display') === 'flex') {
+            $(href).css('display', 'none');
         }
-        form.style.display = 'flex';
-    }
-    $('html,body').animate({scrollTop:$(form).offset().top}, 'slow');
-}
-
-function maj(idRoom, type) {
-    var id = type + idRoom;
-    var element = document.getElementById(id);
-    if (element.type === 'checkbox') {
-        if (element.checked === true) {
+        else {
+            $('.'+$(href).attr('class')).css('display', 'none');
+            $(href).css('display', 'flex');
+        }
+        $('html,body').animate({scrollTop: $(href).offset().top}, 'slow');
+    });
+    $(':checkbox').change(function () {
+        var type = $(this).attr('name');
+        var idRoom = ($(this).attr('id')).split(type)[1];
+        if ($(this).is(':checked')) {
             var state = 1;
         }
         else {
             var state = 0;
         }
-        if (type === 'volet_state') {
-            var actuator = 'volet';
-        }
-        else if (type === 'volet_auto') {
-            var actuator = 'auto';
-        }
-        else {
-            var actuator = 'lumière';
-        }
         $.ajax('model/homeTreatment.php', {
             type: 'POST',
-            data: {idRoom: idRoom, type: actuator, state: state}
+            data: {idRoom: idRoom, type: type, state: state}
         });
-    }
-    else if (element.type === 'time') {
-        var time = element.value;
-        if (type === 'volet_opening') {
-            var actuator = 'opening';
-        }
-        else if (type === 'volet_closing') {
-            var actuator = 'closing';
-        }
+    });
+    $('[type=time]').change(function () {
+        var type = $(this).attr('name');
+        var idRoom = ($(this).attr('id')).split(type)[1];
+        var time = $(this).val();
         $.ajax('model/homeTreatment.php', {
             type: 'POST',
-            data: {idRoom: idRoom, type: actuator, time: time}
+            data: {idRoom: idRoom, type: type, time: time}
         });
-    }
-    else if (element.type === 'number') {
-        var value = element.value;
-        var actuator = type;
+    });
+    $('[type=number]').change(function () {
+        var type = $(this).attr('name');
+        var idRoom = ($(this).attr('id')).split(type)[1];
+        var value = $(this).val();
         $.ajax('model/homeTreatment.php', {
             type: 'POST',
-            data: {idRoom: idRoom, type: actuator, value: value}
+            data: {idRoom: idRoom, type: type, value: value}
         });
-    }
-}
+    });
+});
